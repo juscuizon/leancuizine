6668baab731103cacec93b71e6a3a199dae56d8d
WFJ Cuizon <justincuizon@gmail.com>
2017-03-18 07:32:54 +0800

    Added links to navigation

---
+++ assets/js/active-menu-toggle.js
@@ -0,0 +1,10 @@
+(function() {
+    var nav = document.getElementById('nav'),
+        anchor = nav.getElementsByTagName('a'),
+        current = window.location.pathname.split('/')[1];
+        for (var i = 0; i < anchor.length; i++) {
+        if(anchor[i].href == current) {
+            anchor[i].className = "active";
+        }
+    }
+})();
